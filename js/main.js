4ab836d3432e4d8ee106f0a2c4f6f2dd9d5f8469
David Hazinski <dhazinski@gmail.com>
2011-09-10 15:40:04 -0400

    Formatting nav menu.

---
+++ js/main.js
@@ -0,0 +1,16 @@
+$(function(){
+
+    // Setup menu backgrounds
+    $('.nav-menu > li > a').each(function(index) {
+        var px = 100 + 6 * index;
+        $(this).css('left', px * -1 + 'px');
+        $(this).find('.nav-menu-item').css('left', px + 'px');
+    });
+
+    // Setup sub-menu show/hide
+    $('.nav-sub-menu').parent().click(function() {
+        $(this).children('.nav-sub-menu').slideToggle();
+    });
+    $('.nav-sub-menu').hide();
+
+});
