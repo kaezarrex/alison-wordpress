1f687cdecac26c882c798f364dad7dc9f32d0c83
David Hazinski <dhazinski@gmail.com>
2011-10-19 23:07:48 -0400

    Hooking up navigation links.

--- sidebar.php
+++ sidebar.php
@@ -1,14 +1,12 @@
             <nav>
                 <ul class="nav-menu">
-                    <li><a href="#"><div class="nav-menu-item">About</div></a></li>
+                    <li><a href="http://alisoncitron.com/index.html"><div class="nav-menu-item">About</div></a></li>
                     <li>
                         <a href="#"><div class="nav-menu-item"><?php _e( 'Categories', 'sandbox' ) ?></div></a>
                         <ul class="nav-sub-menu">
                             <?php wp_list_categories('title_li=&show_count=0&hierarchical=1') ?> 
                         </ul>
                     </li>
-                    <li><a href="#"><div class="nav-menu-item">Press</div></a></li>
-                    <li><a href="#"><div class="nav-menu-item">Shop</div></a></li>
-                    <li><a href="#"><div class="nav-menu-item">Newsletter</div></a></li>
+                    <li><a href="http://www.etsy.com/shop/LoveCitron"><div class="nav-menu-item">Shop</div></a></li>
                 </ul>
             </nav>
