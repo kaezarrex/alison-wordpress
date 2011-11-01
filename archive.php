991bfe2564df28cd07edb6d6615d5ad29f9917ea
David Hazinski <dhazinski@gmail.com>
2011-10-31 23:04:58 -0400

    Actually styling archive.

--- archive.php
+++ archive.php
@@ -1,56 +1,76 @@
 <?php get_header() ?>
 
-	<div id="container">
-		<div id="content">
+    <div id="content">
+
+<?php get_sidebar() ?>
 
 <?php the_post() ?>
 
 <?php if ( is_day() ) : ?>
-			<h2 class="page-title"><?php printf( __( 'Daily Archives: <span>%s</span>', 'sandbox' ), get_the_time(get_option('date_format')) ) ?></h2>
+        <h1 class="page-title"><?php printf( __( 'Daily Archives: <span>%s</span>', 'sandbox' ), get_the_time(get_option('date_format')) ) ?></h1>
 <?php elseif ( is_month() ) : ?>
-			<h2 class="page-title"><?php printf( __( 'Monthly Archives: <span>%s</span>', 'sandbox' ), get_the_time('F Y') ) ?></h2>
+        <h1 class="page-title"><?php printf( __( 'Monthly Archives: <span>%s</span>', 'sandbox' ), get_the_time('F Y') ) ?></h1>
 <?php elseif ( is_year() ) : ?>
-			<h2 class="page-title"><?php printf( __( 'Yearly Archives: <span>%s</span>', 'sandbox' ), get_the_time('Y') ) ?></h2>
+        <h1 class="page-title"><?php printf( __( 'Yearly Archives: <span>%s</span>', 'sandbox' ), get_the_time('Y') ) ?></h1>
 <?php elseif ( isset($_GET['paged']) && !empty($_GET['paged']) ) : ?>
-			<h2 class="page-title"><?php _e( 'Blog Archives', 'sandbox' ) ?></h2>
+        <h1 class="page-title"><?php _e( 'Blog Archives', 'sandbox' ) ?></h1>
 <?php endif; ?>
 
 <?php rewind_posts() ?>
 
-			<div id="nav-above" class="navigation">
-				<div class="nav-previous"><?php next_posts_link( __( '<span class="meta-nav">&laquo;</span> Older posts', 'sandbox' ) ) ?></div>
-				<div class="nav-next"><?php previous_posts_link( __( 'Newer posts <span class="meta-nav">&raquo;</span>', 'sandbox' ) ) ?></div>
-			</div>
+            <section id="nav-above" class="navigation">
+                <?php next_posts_link( '%link', '<div class="nav-previous">%title</div>' ) ?>
+                <?php previous_posts_link( '%link', '<div class="nav-next">%title</div>' ) ?>
+            </section>
+
+            <section id="posts">
 
 <?php while ( have_posts() ) : the_post(); ?>
 
-			<div id="post-<?php the_ID() ?>" class="<?php sandbox_post_class() ?>">
-				<h3 class="entry-title"><a href="<?php the_permalink() ?>" title="<?php printf( __( 'Permalink to %s', 'sandbox' ), the_title_attribute('echo=0') ) ?>" rel="bookmark"><?php the_title() ?></a></h3>
-				<div class="entry-date"><abbr class="published" title="<?php the_time('Y-m-d\TH:i:sO') ?>"><?php unset($previousday); printf( __( '%1$s &#8211; %2$s', 'sandbox' ), the_date( '', '', '', false ), get_the_time() ) ?></abbr></div>
-				<div class="entry-content">
-<?php the_excerpt( __( 'Read More <span class="meta-nav">&raquo;</span>', 'sandbox' ) ) ?>
-
-				</div>
-				<div class="entry-meta">
-					<span class="author vcard"><?php printf( __( 'By %s', 'sandbox' ), '<a class="url fn n" href="' . get_author_link( false, $authordata->ID, $authordata->user_nicename ) . '" title="' . sprintf( __( 'View all posts by %s', 'sandbox' ), $authordata->display_name ) . '">' . get_the_author() . '</a>' ) ?></span>
-					<span class="meta-sep">|</span>
-					<span class="cat-links"><?php printf( __( 'Posted in %s', 'sandbox' ), get_the_category_list(', ') ) ?></span>
-					<span class="meta-sep">|</span>
-					<?php the_tags( __( '<span class="tag-links">Tagged ', 'sandbox' ), ", ", "</span>\n\t\t\t\t\t<span class=\"meta-sep\">|</span>\n" ) ?>
-<?php edit_post_link( __( 'Edit', 'sandbox' ), "\t\t\t\t\t<span class=\"edit-link\">", "</span>\n\t\t\t\t\t<span class=\"meta-sep\">|</span>\n" ) ?>
-					<span class="comments-link"><?php comments_popup_link( __( 'Comments (0)', 'sandbox' ), __( 'Comments (1)', 'sandbox' ), __( 'Comments (%)', 'sandbox' ) ) ?></span>
-				</div>
-			</div><!-- .post -->
+                <article id="post-<?php the_ID() ?>" class="<?php sandbox_post_class() ?>">
+                    <header>
+                        <div class="date">
+                            <span class="badge"><?php the_time('j') ?></span>
+                            <span class="month"><?php the_time('M') ?> &#8211; <?php the_time() ?></span>
+                        </div>
+                        <h1>
+                            <a href="<?php the_permalink() ?>" title="<?php printf( __('Permalink to %s', 'sandbox'), the_title_attribute('echo=0') ) ?>" rel="bookmark">
+                                <?php the_title() ?>
+                            </a>
+                        </h1>
+                    </header>
+                    <section>
+                        <?php the_excerpt(__( 'Read More <span class="meta-nav">&raquo;</span>', 'sandbox' )) ?>
+                    </section>
+                    <footer>
+                        <section>
+                            <h2>
+                                <?php comments_popup_link( __( 'Comments (0)', 'sandbox' ), __( 'Comments (1)', 'sandbox' ), __( 'Comments (%)', 'sandbox' ) ) ?>
+                            </h2>
+                        </section>
+                        <div class="slash"></div>
+                        <section>
+                            <h2>Categories</h2>
+                            <h3><?php echo(get_the_category_list(', ')) ?></h3>
+                        </section>
+                        <div class="slash"></div>
+                        <section>
+                            <h2>Author</h2>
+                            <h3><?php echo(get_the_author()) ?></h3>
+                        </section>
+                    </footer>
+                </article><!-- .post -->
 
 <?php endwhile; ?>
 
-			<div id="nav-below" class="navigation">
-				<div class="nav-previous"><?php next_posts_link( __( '<span class="meta-nav">&laquo;</span> Older posts', 'sandbox' ) ) ?></div>
-				<div class="nav-next"><?php previous_posts_link( __( 'Newer posts <span class="meta-nav">&raquo;</span>', 'sandbox' ) ) ?></div>
-			</div>
+            </section>
 
-		</div><!-- #content .hfeed -->
-	</div><!-- #container -->
+            <section id="nav-above" class="navigation">
+                <?php next_posts_link( '%link', '<div class="nav-previous">%title</div>' ) ?>
+                <?php previous_posts_link( '%link', '<div class="nav-next">%title</div>' ) ?>
+            </section>
+
+    </div><!-- #content .hfeed -->
 
 <?php get_sidebar() ?>
 <?php get_footer() ?>
