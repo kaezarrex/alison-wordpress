b283cf0af1e186c947f8a72595bb39f4b3316d1a
David Hazinski <dhazinski@gmail.com>
2013-02-26 02:47:31 +0000

    Adding Google Analytics.

--- footer.php
+++ footer.php
@@ -1,4 +1,4 @@
-	<footer id="main-footer">
+    <footer id="main-footer">
 
         <section>
             <h1>Categories</h1>
@@ -45,7 +45,22 @@
             <span class="meta-sep">|</span>
             <span><?php wp_loginout() ?></span>
         </div>
-	</footer>
+    </footer>
+
+    <script type="text/javascript">
+
+      var _gaq = _gaq || [];
+      _gaq.push(['_setAccount', 'UA-519429-4']);
+      _gaq.push(['_setDomainName', 'alisoncitron.com']);
+      _gaq.push(['_trackPageview']);
+
+      (function() {
+        var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+        ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+        var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+      })();
+
+    </script>
 
 <?php wp_footer() ?>
 
