c6092977ae418a41877c8bdb1cb03eaee9d9a5d4
David Hazinski <dhazinski@gmail.com>
2013-03-04 04:45:08 +0000

    Adding Google Analytics.

--- footer.php
+++ footer.php
@@ -2,6 +2,18 @@
 
         <script type="text/javascript" src="//cdnjs.cloudflare.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
         <script type="text/javascript" src="//cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/2.3.0/js/bootstrap.min.js"></script>
+        <script type="text/javascript">
+            var _gaq = _gaq || [];
+            _gaq.push(['_setAccount', 'UA-519429-4']);
+            _gaq.push(['_setDomainName', 'alisoncitron.com']);
+            _gaq.push(['_trackPageview']);
+
+            (function() {
+                var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+                ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+                var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+            })();
+        </script>
 
         <?php wp_footer(); ?>
     </body>
