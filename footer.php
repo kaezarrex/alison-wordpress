11e29a20254334291078983137c3827793518f95
David Hazinski <dhazinski@gmail.com>
2013-03-04 04:34:07 +0000

    Collapsing category and archive lists.

--- footer.php
+++ footer.php
@@ -1,7 +1,7 @@
         </div><!--container-->
 
+        <script type="text/javascript" src="//cdnjs.cloudflare.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
         <script type="text/javascript" src="//cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/2.3.0/js/bootstrap.min.js"></script>
-        <script type="text/javascript" src="<?php bloginfo('stylesheet_directory'); ?>/js/main.js"></script>
 
         <?php wp_footer(); ?>
     </body>
--- sidebar.php
+++ sidebar.php
@@ -12,14 +12,14 @@
                     <li><h2><a href="#" class="social-facebook">Facebook</a></h2></li>
                     <li><h2><a href="#" class="social-twitter">Twitter</a></h2></li>
                 </ul>
-                <h3 id="categories">Search</h3>
+                <h3 id="search">Search</h3>
                 <?php get_search_form(); ?>
-                <h3 id="categories">Categories</h3>
-                <ul class="sidebar-list unstyled">
+                <h3 id="categories"><a href="#sidebar-category-list" data-toggle="collapse">Categories</a></h3>
+                <ul id="sidebar-category-list" class="sidebar-list unstyled collapse">
                     <?php wp_list_cats(); ?>
                 </ul>
-                <h3 id="archives">Archives</h3>
-                <ul class="sidebar-list unstyled">
+                <h3 id="archives"><a href="#sidebar-archive-list" data-toggle="collapse">Archives</a></h3>
+                <ul id="sidebar-archive-list" class="sidebar-list unstyled collapse">
                     <?php wp_get_archives('type=monthly') ?>
                 </ul>
             </div>
