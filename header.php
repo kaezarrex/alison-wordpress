f7fd5996b83c91963dc1ae4a81a451e9b75a2ae7
David Hazinski <dhazinski@gmail.com>
2013-03-04 04:46:00 +0000

    Removing unused CSS import.

--- header.php
+++ header.php
@@ -6,7 +6,6 @@
         <link rel="icon" type="image/png" href="<?php bloginfo('stylesheet_directory'); ?>/images/favicon.png">
         <link rel="stylesheet" type="text/css" href="//cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/2.3.0/css/bootstrap.min.css" />
         <link rel="stylesheet" type="text/css" href="<?php bloginfo('stylesheet_url') ?>" />
-        <link rel="stylesheet" type="text/css" href="<?php bloginfo('stylesheet_directory'); ?>/css/main.css" />
         <script type="text/javascript" src="//use.typekit.net/miw4xkh.js"></script>
         <script type="text/javascript">try{Typekit.load();}catch(e){}</script>
         <?php wp_head(); ?>
