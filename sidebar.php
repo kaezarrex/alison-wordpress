3835f467ea5ef2a7caccba32769caa3478fdad33
David Hazinski <dhazinski@gmail.com>
2013-03-04 04:50:08 +0000

    Adding space between sidebar sections.

--- sidebar.php
+++ sidebar.php
@@ -1,25 +1,33 @@
 
             <div id="sidebar-content">
-                <div id="bio">
+                <div id="bio" class="sidebar-section">
                     <!--<img src="http://placekitten.com/160/120">-->
                     <div id="bio-headline">I'm Alison Citron.</div>
                     <div id="bio-body">This is where I talk about my design life adventures.</div>
                 </div>
-                <ul class="unstyled">
-                    <!--<li><h2><a href="#" class="social-newsletter">Newsletter</a></h2></li>-->
-                    <li><h2><a href="http://pinterest.com/thealisoncitron/" class="social-pinterest">Pinterest</a></h2></li>
-                    <li><h2><a href="http://instagram.com/thealisoncitron" class="social-instagram">Instagram</a></h2></li>
-                    <li><h2><a href="http://www.facebook.com/pages/Love-Citron/308198629206234" class="social-facebook">Facebook</a></h2></li>
-                    <li><h2><a href="https://twitter.com/thealisoncitron" class="social-twitter">Twitter</a></h2></li>
-                </ul>
-                <h3 id="search">Search</h3>
-                <?php get_search_form(); ?>
-                <h3 id="categories"><a href="#sidebar-category-list" data-toggle="collapse">Categories</a></h3>
-                <ul id="sidebar-category-list" class="sidebar-list unstyled collapse">
-                    <?php wp_list_cats(); ?>
-                </ul>
-                <h3 id="archives"><a href="#sidebar-archive-list" data-toggle="collapse">Archives</a></h3>
-                <ul id="sidebar-archive-list" class="sidebar-list unstyled collapse">
-                    <?php wp_get_archives('type=monthly') ?>
-                </ul>
+                <div class="sidebar-section">
+                    <ul class="unstyled">
+                        <!--<li><h2><a href="#" class="social-newsletter">Newsletter</a></h2></li>-->
+                        <li><h2><a href="http://pinterest.com/thealisoncitron/" class="social-pinterest">Pinterest</a></h2></li>
+                        <li><h2><a href="http://instagram.com/thealisoncitron" class="social-instagram">Instagram</a></h2></li>
+                        <li><h2><a href="http://www.facebook.com/pages/Love-Citron/308198629206234" class="social-facebook">Facebook</a></h2></li>
+                        <li><h2><a href="https://twitter.com/thealisoncitron" class="social-twitter">Twitter</a></h2></li>
+                    </ul>
+                </div>
+                <div class="sidebar-section">
+                    <h3 id="search">Search</h3>
+                    <?php get_search_form(); ?>
+                </div>
+                <div class="sidebar-section">
+                    <h3 id="categories"><a href="#sidebar-category-list" data-toggle="collapse">Categories</a></h3>
+                    <ul id="sidebar-category-list" class="sidebar-list unstyled collapse">
+                        <?php wp_list_cats(); ?>
+                    </ul>
+                </div>
+                <div class="sidebar-section">
+                    <h3 id="archives"><a href="#sidebar-archive-list" data-toggle="collapse">Archives</a></h3>
+                    <ul id="sidebar-archive-list" class="sidebar-list unstyled collapse">
+                        <?php wp_get_archives('type=monthly') ?>
+                    </ul>
+                </div>
             </div>
