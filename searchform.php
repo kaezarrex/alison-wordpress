bc2ddc1ad4bfeddfd29a71a9497e21082f87aeea
David Hazinski <dhazinski@gmail.com>
2013-03-04 06:05:15 +0000

    Fixing search form submission.

--- searchform.php
+++ searchform.php
@@ -1,4 +1,4 @@
-<form action="/" method="get">
+<form action="<?php echo home_url() ?>" method="get">
     <input class="span2" type="text" name="s" value="<?php the_search_query(); ?>">
     <button type="submit" class="btn">Go!</button>
 </form>
