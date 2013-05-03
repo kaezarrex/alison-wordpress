aa92747e6c56f0f2913a2093561f0c35435aed1d
David Hazinski <dhazinski@gmail.com>
2013-05-03 09:10:41 -0400

    Moving the Google Analytics tracking from footer to head.

--- footer.php
+++ footer.php
@@ -4,18 +4,6 @@
         <script type="text/javascript" src="//cdnjs.cloudflare.com/ajax/libs/underscore.js/1.4.4/underscore-min.js"></script>
         <script type="text/javascript" src="//cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/2.3.0/js/bootstrap.min.js"></script>
         <script type="text/javascript" src="<?php bloginfo('stylesheet_directory'); ?>/js/pinit.js"></script>
-        <script type="text/javascript">
-            var _gaq = _gaq || [];
-            _gaq.push(['_setAccount', 'UA-519429-4']);
-            _gaq.push(['_setDomainName', 'alisoncitron.com']);
-            _gaq.push(['_trackPageview']);
-
-            (function() {
-                var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-                ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-                var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-            })();
-        </script>
 
         <?php wp_footer(); ?>
     </body>
--- header.php
+++ header.php
@@ -8,6 +8,18 @@
         <link rel="stylesheet" type="text/css" href="<?php bloginfo('stylesheet_url') ?>" />
         <script type="text/javascript" src="//use.typekit.net/miw4xkh.js"></script>
         <script type="text/javascript">try{Typekit.load();}catch(e){}</script>
+        <script type="text/javascript">
+            var _gaq = _gaq || [];
+            _gaq.push(['_setAccount', 'UA-519429-4']);
+            _gaq.push(['_setDomainName', 'alisoncitron.com']);
+            _gaq.push(['_trackPageview']);
+
+            (function() {
+                var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+                ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+                var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+            })();
+        </script>
         <?php wp_head(); ?>
     </head>
 
