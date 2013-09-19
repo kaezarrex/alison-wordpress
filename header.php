c3abb47b63fddc1608a7e0588419b07911155cb9
David Hazinski <dhazinski@gmail.com>
2013-09-18 23:24:17 -0400

    Updating nav to match new site design.

--- header.php
+++ header.php
@@ -28,11 +28,12 @@
         <div id="top-menu" class="navbar-fixed-top">
             <div class="container">
                 <div class="row">
-                    <div class="span2 menu-item"><a href="//oneandonly.im/pricing">Pricing</a></div>
                     <div class="span2 menu-item"><a href="//oneandonly.im/gettingmarried">Weddings</a></div>
+                    <div class="span2 menu-item"><a href="//oneandonly.im/interested">Work With Me</a></div>
                     <div class="span4"><a href="//oneandonly.im"><img src="//oneandonly.im/media/img/oneandonlylogo.png"></a></div>
-                    <div class="span2 menu-item"><a href="//alisoncitron.com/blog">Blog</a></div>
-                    <div class="span2 menu-item"><a href="//oneandonly.im/work">Work</a></div>
+                    <div class="menu-item"><a href="//oneandonly.im/work">Work</a></div>
+                    <div class="menu-item"><a href="//alisoncitron.com/blog">Blog</a></div>
+                    <div class="menu-item"><a href="//oneandonly.im/about">About</a></div>
                 </div>
             </div>
         </div>
